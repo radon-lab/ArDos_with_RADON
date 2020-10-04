e7eb14d3c9b42645e8f07f80435a54c4c54ab1cd
radon-lab <71728333+radon-lab@users.noreply.github.com>
2020-10-04 10:50:17 +0300

    3.0.5
    Убрал учет собственного фона из расчета текущего фона. Добавил динамическое изменение времени счета.

--- ArDos_with_RADON/ArDos_with_RADON.ino
+++ ArDos_with_RADON/ArDos_with_RADON.ino
@@ -1,5 +1,5 @@
 /*Arduino IDE 1.8.12
-  Версия программы RADON v3.0.5 low_pwr 03.10.20 специально для проекта ArDos
+  Версия программы RADON v3.0.5 low_pwr 04.10.20 специально для проекта ArDos
   Страница проекта ArDos http://arduino.ru/forum/proekty/delaem-dozimetr
   Желательна установка лёгкого ядра https://alexgyver.github.io/package_GyverCore_index.json и загрузчика OptiBoot v8 https://github.com/Optiboot/optiboot
 
